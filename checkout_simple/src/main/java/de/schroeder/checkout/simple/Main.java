b3cae4bd4d41243c2dcf29eaa21cbea2ffa17514
schroeder <r.schroeder@business-keeper.com>
2016-07-05 14:50:49 +0200

    initial commit

--- checkout_simple/src/main/java/de/schroeder/checkout/simple/Main.java
+++ checkout_simple/src/main/java/de/schroeder/checkout/simple/Main.java
@@ -1,8 +1,9 @@
 package de.schroeder.checkout.simple;
 
+import de.schroeder.checkout.simple.service.SetupFactory;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
     }
 }
